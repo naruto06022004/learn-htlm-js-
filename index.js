4edd3aac2c0c82b0da347d2be23abfd247ac073c
MSI\PC <tuyen01668686162@gmail.com>
2023-06-25 22:03:51 +0700

    sunday night commit

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
+//console.log(isLandscape (20, 25));
+//function isLandscape(width , height ) {
+ // return (width < height) ;
+//} 
+//function out fizzBuzz();
+//checkSpeed(70);
+/////function checkSpeed(speed) {
+   /// const speedLimit = 70;
+   // const kmPerPoint = 5;
+   // if (speed <= speedLimit + kmPerPoint) 
+//console.log('Ok');
+//else { 
+   // const poinst = Math.floor((speed - speedLimit) / kmPerPoint);
+//if ( points >= 12) 
+//console.log('Licese suppended');
+ //else console.log('Points',points);
+    
 
-let points = 110;
-let type > 100 ? 'gold' : 'siler ';
-console.log(type);
+//showNumbers(10);
+//function showNumbers (limit) {
+    //for (let i = 0; i <= limit ; i ++ ){
+    //const message = (i % 2 === 0 )? 'EVEN' : 'ODD'
+    //console.log(i,message);
+    //}
+//}
+const array = [0,1 ]
+console.log (coutTruthy(array))
+function coutTruthy (array){
+   let count = 0 ;
+   for (let value of array)
+   if (value)
+   count ++
+   return count; 
+}
