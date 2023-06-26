511ea2a085b617344d3f5e89cabda9264e257520
MSI\PC <tuyen01668686162@gmail.com>
2023-06-26 22:31:33 +0700

    monday

--- index.js
+++ index.js
@@ -1,34 +1,61 @@
-//console.log(isLandscape (20, 25));
-//function isLandscape(width , height ) {
- // return (width < height) ;
-//} 
-//function out fizzBuzz();
-//checkSpeed(70);
-/////function checkSpeed(speed) {
-   /// const speedLimit = 70;
-   // const kmPerPoint = 5;
-   // if (speed <= speedLimit + kmPerPoint) 
-//console.log('Ok');
-//else { 
-   // const poinst = Math.floor((speed - speedLimit) / kmPerPoint);
-//if ( points >= 12) 
-//console.log('Licese suppended');
- //else console.log('Points',points);
-    
+const movie = {
+    title: 'a',
+    releaseYear  : 2018,
+    reting: 4.5,
+    director:'b'
+};
 
-//showNumbers(10);
-//function showNumbers (limit) {
-    //for (let i = 0; i <= limit ; i ++ ){
-    //const message = (i % 2 === 0 )? 'EVEN' : 'ODD'
-    //console.log(i,message);
-    //}
-//}
-const array = [0,1 ]
-console.log (coutTruthy(array))
-function coutTruthy (array){
-   let count = 0 ;
-   for (let value of array)
-   if (value)
-   count ++
-   return count; 
+showProperties(movie);
+function showProperties(obj) {
+    for(let key in obj )
+        if (typeof obj[key]==='string')
+        console.log(key ,obj[key]);
+    
 }
+// bai 2
+showStars(4);
+function showStars(row){
+    // 4
+    for(let i = 1 ; i <= row; i++ ){
+        let pattern = '';
+         for (let i = 0 ; i< row ;i++ )
+      pattern += '*'
+      console.log(pattern);
+}
+}
+// bai 3
+
+// prime (whose fac are only 1 and it s)
+// coposite
+//12 = 1,2,3,4,6,12
+// con be divided evenly by factors 
+// 11 = 1 , 11
+// 13 = 1,13
+showPrimes (54);
+
+function showPrimes(limit) {
+    for (let number = 2; number <= limit ; number ++ ){
+    // 2- current 
+    let Primes = true;
+    for (let factor = 2 ; factor <= number ; factor++){
+        if (number % factor === 0) {
+        isPrime = false;
+        break
+    }
+ }
+ if (isPrime) console.log(number);
+}
+}
+   
+function isPrime(number){
+        let Primes = true;
+        for (let factor = 2 ; factor <= number ; factor++){
+            if (number % factor === 0) {
+            isPrime = false;
+            return false;
+
+    }
+}
+return true;
+    }
+    
